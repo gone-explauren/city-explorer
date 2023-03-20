eeccd42c9283f93751c749469e54ab3911d04cea
Laurel Perkins <123340286+gone-explauren@users.noreply.github.com>
2023-03-19 17:08:48 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -16,7 +16,6 @@ class App extends React.Component {
       latitude: '',
       longitude: '',
       showMap: false,
-      mapURL: `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&center=${this.state.latitude},${this.state.longitude}`,
 
       weatherData: [],
       showWeather: false,
@@ -24,11 +23,50 @@ class App extends React.Component {
       movieData: [],
       showMovieInfo: false,
 
-      error: false,
-      errorMessage: ''
+      error: false
     }
   }
 
+  weatherSubmit = async () => {
+
+    let weatherURL = `${process.env.REACT_APP_SERVER}/weather?search=${this.state.cityName}&lat=${this.state.latitude}&lon=${this.state.longitude}`
+
+    try {
+      let weatherData = await axios.get(weatherURL)
+      this.setState({
+        weatherData: weatherData.data,
+        showWeather: true
+      })
+
+    } catch (error) {
+      console.log(error);
+      this.setState({
+        error: true,
+        errorMessage: `We oops'd... Error: ${error.response.status}`
+      });
+    }
+
+  }
+
+  movieSubmit = async () => {
+    // console.log("you're watching movies!");
+    let movieURL = `${process.env.REACT_APP_SERVER}/movies?search=${this.state.cityName}`;
+
+    try {
+
+      let movieData = await axios.get(movieURL)
+      this.setState({
+        movieData: movieData.data,
+        showMovieInfo: true
+      }, () => console.log(this.state.movieData))
+
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMessage: `We oops'd... Error: ${error.response.status}`
+      });
+    }
+  }
 
   citySubmit = async (e) => {
     e.preventDefault()
@@ -37,15 +75,18 @@ class App extends React.Component {
     let cityURL = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.cityName}&format=json`;
 
     try {
+
       let cityData = await axios.get(cityURL);
+
       this.setState({
         cityData: cityData.data[0],
-        cityName: cityData.data[0].display_name,
         latitude: cityData.data[0].lat,
         longitude: cityData.data[0].lon,
         showMap: true
-      }, this.weatherSubmit, this.movieSubmit);
-
+      }, () => {
+        this.weatherSubmit();
+        this.movieSubmit();
+      })
 
     } catch (error) {
       this.setState({
@@ -56,39 +97,22 @@ class App extends React.Component {
     // console.log(cityData);
   }
 
-  weatherSubmit = async () => {
-
-    let weatherURL = `${process.env.REACT_APP_SERVER}/weather?search=${this.state.cityName}&lat=${this.state.latitude}&lon=${this.state.longitude}`
-
-    let weatherData = await axios.get(weatherURL)
-    this.setState({
-      weatherData: weatherData.data,
-      showWeather: true
-    })
-  }
-
-  movieSubmit = async () => {
-    let movieURL = `${process.env.REACT_APP_LOCALURL}/movies?search=${this.state.cityNameInput}`;
-
-    let movieData = await axios.get(movieURL)
-    this.setState({
-      movieData: movieData.data,
-      showMovieInfo: true
-    })
-  }
-
-
   handleInput = (e) => {
     this.setState({
       cityName: e.target.value
     });
   }
 
-
   render() {
 
-    return (
+    let movies = this.state.movieData.map(movie => { 
+      console.log(movie);
+      return <Movie singleMovieData={movie} />
+    })
+    movies = movies.splice(0, 3);
 
+
+    return (
       <>
         <header>
           <h1>Laurel Explaurel</h1>
@@ -108,43 +132,30 @@ class App extends React.Component {
               latitude={this.state.latitude}
               longitude={this.state.longitude}
               showMap={this.state.showMap}
-              mapURL={this.state.mapURL}
-
-              citySubmit={this.citySubmit}
-
-              error={this.state.error}
-              errorMessage={this.state.errorMessage}
             />
 
             <Weather
-              cityData={this.state.cityData}
               cityName={this.state.cityName}
               latitude={this.state.latitude}
               longitude={this.state.longitude}
 
               weatherData={this.state.weatherData}
               showWeather={this.state.showWeather}
-
-              citySubmit={this.citySubmit}
-              weatherSubmit={this.weatherSubmit}
-
-              error={this.state.error}
-              errorMessage={this.state.errorMessage}
             />
 
-            <Movie
-              cityData={this.state.cityData}
+            {movies?.length !== 0
+              && <>
+                <h3>Movies:</h3>
+                {movies}
+              </>
+            }
+
+            {/* <Movie
               cityName={this.state.cityName}
 
               movieData={this.state.movieData}
               showMovieInfo={this.state.showMovieInfo}
-
-              citySubmit={this.citySubmit}
-              movieSubmit={this.movieSubmit}
-              
-              error={this.state.error}
-              errorMessage={this.state.errorMessage}
-            />
+            /> */}
 
           </>
         </main>
@@ -153,9 +164,7 @@ class App extends React.Component {
           <p>Copyrght Laurel Perkins, 2023</p>
         </footer>
       </>
-
     );
-
   }
 }
 
